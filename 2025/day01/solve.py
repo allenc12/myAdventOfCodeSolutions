e5a3dee13df369e82b1d58e054390fc2930aef33
Clayton Allen <mrchafeits@aaathats3as.com>
2025-11-30 21:57:59 -0800

    lets see if part 2 works i guess

--- 2025/day01/solve.py
+++ 2025/day01/solve.py
@@ -1,6 +1,6 @@
+import operator
 
-
-def part1(lines: list[str]) -> None:
+def part1(lines: list[str]) -> int:
     times_on_zero = 0
     dial_index = 50
     for rotation in lines:
@@ -14,7 +14,33 @@ def part1(lines: list[str]) -> None:
             times_on_zero += 1
     return times_on_zero
 
+def part2(lines: list[str]) -> int:
+    times_passed_zero = 0
+    dial_index = 50
+    new_index = 0
+    for rotation in lines:
+        rotate_by = int(rotation[1:])
+        if rotation[0] == 'R':
+            new_index = (dial_index + rotate_by)
+        else: # assume it's 'L'
+            new_index = (dial_index - rotate_by)
+        if new_index >= 100 or new_index < 0 or new_index == 0:
+            times_passed_zero += (abs(new_index) // 100) + 1 if new_index%100==0 else 0
+        dial_index = new_index % 100
+    return times_passed_zero
 
+
+# def part2_gubgar_method(lines: list[str]) -> int:
+#     ret = 0
+#     idx = 50
+#     rot = None
+#     for l in lines:
+#         d,v = l[0], int(l[1:])
+#         if d == "R":
+#             rot = operator.add
+#         else:
+#             rot = operator.sub
+        
 
 test1 = """L68
 L30
@@ -26,7 +52,15 @@ L1
 L99
 R14
 L82""".splitlines()
-print(part1(test1))
+print("test 1 (expect 3): ", part1(test1))
 
 puzzle_input = open("2025/inputs/day01.txt").read().splitlines()
-print(part1(puzzle_input))
+print("part 1: ", part1(puzzle_input))
+
+test2 = """R1000
+L1000
+L50""".splitlines()
+
+print("part2(test1) (expect 6): ", part2(test1))
+print("part2(test2) (expect 10): ", part2(test2))
+print("part 2: ", part2(puzzle_input))
