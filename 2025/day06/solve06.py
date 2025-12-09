11a4f921625c72b76c08c7089d868199d841cc7e
Clayton Allen <mrchafeits@aaathats3as.com>
2025-12-09 15:00:10 -0800

    day06 part 1 complete, renaming solution scripts

--- 2025/day06/solve06.py
+++ 2025/day06/solve06.py
@@ -1,3 +1,4 @@
+from typing import LiteralString, Sequence
 from aoc_utils import aoc_utils
 import functools
 import operator
@@ -6,19 +7,28 @@ import numpy as np
 problem_input = aoc_utils.fetch_and_save(2025, 6)
 
 class MathsHomework:
-    def __init__(self, math_problems: list[str]):
+    def __init__(self, math_problems: Sequence[str|LiteralString]):
         self.numbers = []
         self.operations = []
         for l in math_problems[:-1]:
             self.numbers.append(list(map(int, filter(lambda x: len(x) > 0, l.split(" ")))))
+        self.matrix = np.ndarray(np.shape(self.numbers), dtype=np.int64)
+        for i,e in enumerate(self.numbers):
+            for j,k in enumerate(e):
+                self.matrix[i,j] = k
         self.numbers = list(zip(*self.numbers))
+        # self.operations = []
         self.operations = list(map(lambda y: operator.add if y == '+' else operator.mul, 
-                                   filter(lambda x: len(x) > 0, test_str1[-1].split(" "))))
+                                   filter(lambda x: len(x) > 0, math_problems[-1].split(" "))))
+        print(self.matrix.shape)
         
     def part1_sum(self, _):
         results = []
         for i,op in enumerate(self.operations):
-            results.append(functools.reduce(op, self.numbers[i]))
+            initial_value = 0 if op == operator.add else 1
+            # print(f"{self.matrix[..., i]}  {op}")
+            # results.append(functools.reduce(op, self.matrix[..., i], initial_value))
+            results.append(functools.reduce(op, self.numbers[i], initial_value))
         return sum(results)
 
 
@@ -37,8 +47,7 @@ test_cases = [
         'input': """123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   +  
-""",
+*   +   *   +  """,
         'output': 4277556,
     }
 ]
@@ -46,8 +55,7 @@ test_cases = [
 test_str1 = """123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   +  
-""".splitlines()
+*   +   *   +  """.splitlines()
 
 
 def answer(inputs="", level=0, test=False):
@@ -55,10 +63,11 @@ def answer(inputs="", level=0, test=False):
     return homework.part1_sum(test)
 
 
-# t1 = MathsHomework(test_str1)
-# check_test(t1.part1_sum, None, 4277556)
-aoc_utils.test(answer, test_cases)
-# maths_work = MathsHomework(problem_input.splitlines())
+t1 = MathsHomework(test_str1)
+check_test(t1.part1_sum, None, 4277556)
+# aoc_utils.test(answer, test_cases)
+if problem_input is not None:
+    maths_work = MathsHomework(problem_input.splitlines())
 # """
 # 1725757359 too low"""
-# check_test(maths_work.part1_sum, None, expected_result=1725757360)
+    check_test(maths_work.part1_sum, None, expected_result=1725757360)
